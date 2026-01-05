4d86f452f5b8d442e646d10ac961516f72b60018
incognitocoded <nazarperec18@gmail.com>
2026-01-05 18:15:51 +0200

    Update loader script URL to new repository

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/incognitocoded/LostHub/main/Loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/incognitocoded/RIVALS/main/Loader.lua"))()
