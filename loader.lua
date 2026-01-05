8267cdfa29a9fbe3590b8079ee274320312f562d
incognitocoded <nazarperec18@gmail.com>
2026-01-05 18:18:00 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,16 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/incognitocoded/RIVALS/main/Loader.lua"))()
+-- [[ RIVALS OFFICIAL LOADER ]] --
+-- TG: t.me/losthq
+
+-- ВНИМАНИЕ: Проверь, что твой ник точно incognitocoded
+local raw_url = "https://raw.githubusercontent.com/incognitocoded/RIVALS/main/Main.lua"
+
+local success, result = pcall(function()
+    return game:HttpGet(raw_url)
+end)
+
+if success then
+    loadstring(result)()
+else
+    -- Если не работает, выведет ошибку в консоль (F9)
+    warn("RIVALS Loader Error: " .. tostring(result))
+end
